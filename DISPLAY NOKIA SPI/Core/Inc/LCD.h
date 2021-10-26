348a3b718cb01952520037b629cf2db0864d3c1f
João Vitor de Souza <joao.vitor@meta.com.br>
2021-10-26 17:02:56 -0300

    ESTADO INICIAL
    Após teste no programa Atollic com código antigo (2020), foi adaptado para o programa CubeIDE, fazendo as alterações necessárias a partir do programa disponível no classroom (DispNK-sw). Os pinos do GPIOB foram configurados para output a partir do arquivo '.ioc', e NÃO a partir da função 'LCD5110_GPIO_Config' do LCD.c; além disso, a configuração de portas feita por registradores em 2020 foram trocadas por 'MX_GPIO_Init();', disponível no gpio.c. Assim, estamos no estado inicial, sem SPI e com todos os pinos do display sendo utilizados.

---
+++ DISPLAY NOKIA SPI/Core/Inc/LCD.h
@@ -0,0 +1,40 @@
+void LCD5110_init(void);
+
+void LCD5110_write_char(unsigned char c);
+
+void LCD5110_write_char_reg(unsigned char c);
+
+void LCD5110_clear(void);
+
+void LCD5110_set_XY(unsigned char X, unsigned char Y);
+
+void LCD5110_write_string(char *s);
+
+void LCD5110_write_Dec(unsigned int buffer);
+
+
+void enableSleep();
+		void disableSleep();
+		void update();
+		void clrScr();
+		void fillScr();
+		void invert(int mode);
+		void setPixel(uint16_t x, uint16_t y);
+		void clrPixel(uint16_t x, uint16_t y);
+		void invPixel(uint16_t x, uint16_t y);
+		void invertText(int mode);
+		void print(char *st, int x, int y);
+		//void print(String st, int x, int y);
+		//void printNumI(long num, int x, int y, int length=0, char filler=' ');
+		//void printNumF(double num, byte dec, int x, int y, char divider='.', int length=0, char filler=' ');
+		void setFont(uint8_t* font);
+		void drawBitmap(int x, int y, uint8_t* bitmap, int sx, int sy);
+		void drawLine(int x1, int y1, int x2, int y2);
+		void clrLine(int x1, int y1, int x2, int y2);
+		void drawRect(int x1, int y1, int x2, int y2);
+		void clrRect(int x1, int y1, int x2, int y2);
+		void drawRoundRect(int x1, int y1, int x2, int y2);
+		void clrRoundRect(int x1, int y1, int x2, int y2);
+		void drawCircle(int x, int y, int radius);
+		void clrCircle(int x, int y, int radius);
+		void LCD5110_drawLine(int x1, int y1, int x2, int y2);
---
+++ DISPLAY NOKIA SPI/Core/Src/LCD.c
@@ -0,0 +1,992 @@
+/*Adaptado por Sauer para uso dos alunos da Funda��o Liberato*/
+#include "stm32f4xx.h"
+#include "LCD.h"
+#include "font.h"
+#include "grafic.h"
+
+
+
+#define LCD_CS 	12 		//CE
+#define LCD_RST 10 		//RST
+#define LCD_MO	15		//DIN
+#define LCD_SCK	13      //CLK
+#define LCD_DC	14 		//DO
+#define PORT	GPIOB	//GPIO onde est� o display
+
+#define LEFT 0
+#define RIGHT 9999
+#define CENTER 9998
+
+#define false 0
+#define true 1
+
+ int scrbuf[504];
+
+
+//Define the LCD Operation function
+void LCD5110_LCD_write_byte(unsigned char dat,unsigned char LCD5110_MOde);
+void LCD5110_LCD_delay_ms(unsigned int t);
+
+//Define the hardware operation function
+void LCD5110_GPIO_Config(void);
+void LCD5110_SCK(unsigned char temp);
+void LCD5110_MO(unsigned char temp);
+void LCD5110_CS(unsigned char temp);
+void LCD5110_RST(unsigned char temp);
+void LCD5110_DC(unsigned char temp);
+
+
+
+
+
+void LCD5110_init()
+{
+
+	//LCD5110_GPIO_Config();
+
+	LCD5110_DC(1);//LCD_DC = 1;
+	LCD5110_MO(1);//SPI_MO = 1;
+	LCD5110_SCK(1);//SPI_SCK = 1;
+	LCD5110_CS(1);//SPI_CS = 1;
+
+	LCD5110_RST(0);//LCD_RST = 0;
+	LCD5110_LCD_delay_ms(10);
+	LCD5110_RST(1);//LCD_RST = 1;
+
+	LCD5110_LCD_write_byte(0x21,0);
+	LCD5110_LCD_write_byte(0xC0,0); //B6, c0
+	LCD5110_LCD_write_byte(0x06,0);//coeficiente temperatura
+	LCD5110_LCD_write_byte(0x13,0);
+	LCD5110_LCD_write_byte(0x20,0);
+	LCD5110_clear();
+	LCD5110_LCD_write_byte(0x0c,0);
+}
+
+void LCD5110_LCD_write_byte(unsigned char dat,unsigned char mode)
+{
+	unsigned char i;
+
+	LCD5110_CS(0);//SPI_CS = 0;
+
+	if (0 == mode)
+		LCD5110_DC(0);//LCD_DC = 0;
+	else
+		LCD5110_DC(1);//LCD_DC = 1;
+
+	for(i=0;i<8;i++)
+	{
+		LCD5110_MO(dat & 0x80);//SPI_MO = dat & 0x80;
+		dat = dat<<1;
+		LCD5110_SCK(0);//SPI_SCK = 0;
+		LCD5110_SCK(1);//SPI_SCK = 1;
+	}
+
+	LCD5110_CS(1);//SPI_CS = 1;
+
+}
+
+void LCD5110_write_char(unsigned char c)
+{
+	unsigned char line;
+	unsigned char ch = 0;
+
+	c = c - 32;
+
+	for(line=0;line<6;line++)
+	{
+		ch = font6_8[c][line];
+		LCD5110_LCD_write_byte(ch,1);
+		
+	}
+}
+void LCD5110_write_char_reg(unsigned char c)
+{
+	unsigned char line;
+	unsigned char ch = 0;
+
+	c = c - 32;
+
+	for(line=0;line<6;line++)
+	{
+		ch = ~font6_8[c][line];
+		LCD5110_LCD_write_byte(ch,1);
+		
+	}
+}
+
+void LCD5110_write_string(char *s)
+{
+	unsigned char ch;
+  	while(*s!='\0')
+	{
+		ch = *s;
+		LCD5110_write_char(ch);
+		s++;
+	}
+}
+
+
+void LCD5110_clear()
+{
+	unsigned char i,j;
+	for(i=0;i<6;i++)
+		for(j=0;j<84;j++)
+			LCD5110_LCD_write_byte(0,1);	
+}
+
+void LCD5110_set_XY(unsigned char X,unsigned char Y)
+{
+	unsigned char x;
+	x = 6*X;
+
+	LCD5110_LCD_write_byte(0x40|Y,0);
+	LCD5110_LCD_write_byte(0x80|x,0);
+}
+
+void LCD5110_Write_Dec(unsigned int b)
+{
+
+	unsigned char datas[4];
+
+	datas[0] = b/1000;
+	b = b - datas[0]*1000;
+	datas[1] = b/100;
+	b = b - datas[1]*100;
+	datas[2] = b/10;
+	b = b - datas[2]*10;
+	datas[3] = b;
+
+	datas[0]+=48;
+	datas[1]+=48;
+	datas[2]+=48;
+	datas[3]+=48;
+
+	LCD5110_write_char(datas[0]);
+	LCD5110_write_char(datas[1]);
+	LCD5110_write_char(datas[2]);
+	LCD5110_write_char(datas[3]);
+
+	//a++;
+}
+
+void LCD5110_LCD_delay_ms(unsigned int nCount)
+{
+  unsigned long t;
+	t = nCount * 40000;
+	while(t--);
+}
+
+//void LCD5110_GPIO_Config()
+//{
+//
+//	PORT->MODER|=(1<<(LCD_CS*2));
+//	PORT->MODER|=(1<<(LCD_RST*2));
+//	PORT->MODER|=(1<<(LCD_MO*2));
+//	PORT->MODER|=(1<<(LCD_SCK*2));
+//	PORT->MODER|=(1<<(LCD_DC*2));
+//
+//}
+
+void LCD5110_CS(unsigned char temp)
+{
+	if (temp) PORT->ODR|=1<<LCD_CS;
+	else PORT->ODR&=~(1<<LCD_CS);
+
+
+}
+
+void LCD5110_RST(unsigned char temp)
+{
+	if (temp) PORT->ODR|=1<<LCD_RST;
+	else PORT->ODR&=~(1<<LCD_RST);
+
+}
+
+void LCD5110_DC(unsigned char temp)
+{
+	if (temp) PORT->ODR|=1<<LCD_DC;
+	else PORT->ODR&=~(1<<LCD_DC);
+
+}
+
+void LCD5110_MO(unsigned char temp)
+{
+	if (temp) PORT->ODR|=1<<LCD_MO;
+	else PORT->ODR&=~(1<<LCD_MO);
+
+}
+
+void LCD5110_SCK(unsigned char temp)
+{
+	if (temp) PORT->ODR|=1<<LCD_SCK;
+	else PORT->ODR&=~(1<<LCD_SCK);
+}
+
+/* Para usar a fun��o printf */
+int _write(int file, char *ptr, int len)
+{
+  int i=0;
+  for(i=0 ; i<len ; i++)
+	  LCD5110_write_char(*ptr++);
+  return len;
+}
+
+
+/*
+  LCD5110_Graph.cpp - Arduino/chipKit library support for Nokia 5110 compatible LCDs
+  Copyright (C)2015 Rinky-Dink Electronics, Henning Karlsen. All right reserved
+
+  Basic functionality of this library are based on the demo-code provided by
+  ITead studio. You can find the latest version of the library at
+  http://www.RinkyDinkElectronics.com/
+
+  This library has been made to make it easy to use the Nokia 5110 LCD module
+  as a graphics display on an Arduino or a chipKit.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the CC BY-NC-SA 3.0 license.
+  Please see the included documents for further information.
+
+  Commercial use of this library requires you to buy a license that
+  will allow commercial use. This includes using the library,
+  modified or not, as a tool to sell products.
+
+  The license applies to all part of the library including the
+  examples and tools supplied with the library.
+*/
+
+/*
+void LCD5110_enableSleep()
+{
+   _sleep = true;
+   _LCD_Write(PCD8544_SETYADDR, LCD_COMMAND);
+   _LCD_Write(PCD8544_SETXADDR, LCD_COMMAND);
+   for (int b=0; b<504; b++)
+      _LCD_Write(0, LCD_DATA);
+   _LCD_Write(PCD8544_FUNCTIONSET | PCD8544_POWERDOWN, LCD_COMMAND);
+}
+
+void LCD5110_disableSleep()
+{
+   _sleep = false;
+   _LCD_Write(PCD8544_FUNCTIONSET | PCD8544_EXTENDEDINSTRUCTION, LCD_COMMAND);
+   _LCD_Write(PCD8544_SETVOP | _contrast, LCD_COMMAND);
+   _LCD_Write(PCD8544_SETTEMP | LCD_TEMP, LCD_COMMAND);
+   _LCD_Write(PCD8544_SETBIAS | LCD_BIAS, LCD_COMMAND);
+   _LCD_Write(PCD8544_FUNCTIONSET, LCD_COMMAND);
+   _LCD_Write(PCD8544_DISPLAYCONTROL | PCD8544_DISPLAYNORMAL, LCD_COMMAND);
+   update();
+}
+*/
+void LCD5110_update()
+{
+//   if (_sleep==false)
+//   {
+//      _LCD_Write(PCD8544_SETYADDR, LCD_COMMAND);
+//      _LCD_Write(PCD8544_SETXADDR, LCD_COMMAND);
+	LCD5110_set_XY(0, 0);
+      for (int b=0; b<504; b++){
+    	  LCD5110_LCD_write_byte(scrbuf[b],1);
+      }
+         //_LCD_Write(scrbuf[b], LCD_DATA);
+//   }
+}
+
+void LCD5110_clrScr()
+{
+   for (int c=0; c<504; c++)
+      scrbuf[c]=0;
+}
+
+void LCD5110_fillScr()
+{
+   for (int c=0; c<504; c++)
+      scrbuf[c]=255;
+}
+
+//void LCD5110_invert(int mode)
+//{
+//   if (mode==true)
+//      _LCD_Write(PCD8544_DISPLAYCONTROL | PCD8544_DISPLAYINVERTED, LCD_COMMAND);
+//   else
+//      _LCD_Write(PCD8544_DISPLAYCONTROL | PCD8544_DISPLAYNORMAL, LCD_COMMAND);
+//}
+
+void setPixel(uint16_t x, uint16_t y)
+{
+   int by, bi;
+
+   if ((x>=0) && (x<84) && (y>=0) && (y<48))
+   {
+      by=((y/8)*84)+x;
+      bi=y % 8;
+
+      scrbuf[by]=scrbuf[by] | (1<<bi);
+   }
+}
+
+void LCD5110_clrPixel(uint16_t x, uint16_t y)
+{
+   int by, bi;
+
+   if ((x>=0) && (x<84) && (y>=0) && (y<48))
+   {
+      by=((y/8)*84)+x;
+      bi=y % 8;
+
+      scrbuf[by]=scrbuf[by] & ~(1<<bi);
+   }
+}
+
+void LCD5110_invPixel(uint16_t x, uint16_t y)
+{
+   int by, bi;
+
+   if ((x>=0) && (x<84) && (y>=0) && (y<48))
+   {
+      by=((y/8)*84)+x;
+      bi=y % 8;
+
+      if ((scrbuf[by] & (1<<bi))==0)
+         scrbuf[by]=scrbuf[by] | (1<<bi);
+      else
+         scrbuf[by]=scrbuf[by] & ~(1<<bi);
+   }
+}
+/*
+void LCD5110_invertText(int mode)
+{
+   if (mode==true)
+      cfont.inverted=1;
+   else
+      cfont.inverted=0;
+}
+
+void LCD5110_print(char *st, int x, int y)
+{
+   unsigned char ch;
+   int stl;
+
+   stl = strlen(st);
+   if (x == RIGHT)
+      x = 84-(stl*cfont.x_size);
+   if (x == CENTER)
+      x = (84-(stl*cfont.x_size))/2;
+
+   for (int cnt=0; cnt<stl; cnt++)
+         _print_char(*st++, x + (cnt*(cfont.x_size)), y);
+}
+*/
+/*
+void LCD5110_print(String st, int x, int y)
+{
+   char buf[st.length()+1];
+
+   st.toCharArray(buf, st.length()+1);
+   print(buf, x, y);
+}
+*/
+void LCD5110_printNumI(long num, int x, int y, int length, char filler)
+{
+   char buf[25];
+   char st[27];
+   char neg=false;
+   int c=0, f=0;
+
+   if (num==0)
+   {
+      if (length!=0)
+      {
+         for (c=0; c<(length-1); c++)
+            st[c]=filler;
+         st[c]=48;
+         st[c+1]=0;
+      }
+      else
+      {
+         st[0]=48;
+         st[1]=0;
+      }
+   }
+   else
+   {
+      if (num<0)
+      {
+         neg=true;
+         num=-num;
+      }
+
+      while (num>0)
+      {
+         buf[c]=48+(num % 10);
+         c++;
+         num=(num-(num % 10))/10;
+      }
+      buf[c]=0;
+
+      if (neg)
+      {
+         st[0]=45;
+      }
+
+      if (length>(c+neg))
+      {
+         for (int i=0; i<(length-c-neg); i++)
+         {
+            st[i+neg]=filler;
+            f++;
+         }
+      }
+
+      for (int i=0; i<c; i++)
+      {
+         st[i+neg+f]=buf[c-i-1];
+      }
+      st[c+neg+f]=0;
+
+   }
+
+   print(st,x,y);
+}
+
+void LCD5110_printNumF(double num, char dec, int x, int y, char divider, int length, char filler)
+{
+   char st[27];
+   char neg=false;
+
+   if (num<0)
+      neg = true;
+
+   _convert_float(st, num, length, dec);
+
+   if (divider != '.')
+   {
+      for (int i=0; i<sizeof(st); i++)
+         if (st[i]=='.')
+            st[i]=divider;
+   }
+
+   if (filler != ' ')
+   {
+      if (neg)
+      {
+         st[0]='-';
+         for (int i=1; i<sizeof(st); i++)
+            if ((st[i]==' ') || (st[i]=='-'))
+               st[i]=filler;
+      }
+      else
+      {
+         for (int i=0; i<sizeof(st); i++)
+            if (st[i]==' ')
+               st[i]=filler;
+      }
+   }
+
+   print(st,x,y);
+}
+/*
+void LCD5110_print_char(unsigned char c, int x, int y)
+{
+   if ((cfont.y_size % 8) == 0)
+   {
+      int font_idx = ((c - cfont.offset)*(cfont.x_size*(cfont.y_size/8)))+4;
+      for (int rowcnt=0; rowcnt<(cfont.y_size/8); rowcnt++)
+      {
+         for(int cnt=0; cnt<cfont.x_size; cnt++)
+         {
+            for (int b=0; b<8; b++)
+               if ((fontbyte(font_idx+cnt+(rowcnt*cfont.x_size)) & (1<<b))!=0)
+                  if (cfont.inverted==0)
+                     setPixel(x+cnt, y+(rowcnt*8)+b);
+                  else
+                     clrPixel(x+cnt, y+(rowcnt*8)+b);
+               else
+                  if (cfont.inverted==0)
+                     clrPixel(x+cnt, y+(rowcnt*8)+b);
+                  else
+                     setPixel(x+cnt, y+(rowcnt*8)+b);
+         }
+      }
+   }
+   else
+   {
+      int font_idx = ((c - cfont.offset)*((cfont.x_size*cfont.y_size/8)))+4;
+      int cbyte=fontbyte(font_idx);
+      int cbit=7;
+      for (int cx=0; cx<cfont.x_size; cx++)
+      {
+         for (int cy=0; cy<cfont.y_size; cy++)
+         {
+            if ((cbyte & (1<<cbit)) != 0)
+               if (cfont.inverted==0)
+                  setPixel(x+cx, y+cy);
+               else
+                  clrPixel(x+cx, y+cy);
+            else
+               if (cfont.inverted==0)
+                  clrPixel(x+cx, y+cy);
+               else
+                  setPixel(x+cx, y+cy);
+            cbit--;
+            if (cbit<0)
+            {
+               cbit=7;
+               font_idx++;
+               cbyte=fontbyte(font_idx);
+            }
+         }
+      }
+   }
+}
+
+void LCD5110_setFont(uint8_t* font)
+{
+   cfont.font=font;
+   cfont.x_size=fontbyte(0);
+   cfont.y_size=fontbyte(1);
+   cfont.offset=fontbyte(2);
+   cfont.numchars=fontbyte(3);
+   cfont.inverted=0;
+}
+*/
+void LCD5110_drawHLine(int x, int y, int l)
+{
+   int by, bi;
+
+   if ((x>=0) && (x<84) && (y>=0) && (y<48))
+   {
+      for (int cx=0; cx<l; cx++)
+      {
+         by=((y/8)*84)+x;
+         bi=y % 8;
+
+         scrbuf[by+cx] |= (1<<bi);
+      }
+   }
+   LCD5110_update();
+}
+
+void LCD5110_clrHLine(int x, int y, int l)
+{
+   int by, bi;
+
+   if ((x>=0) && (x<84) && (y>=0) && (y<48))
+   {
+      for (int cx=0; cx<l; cx++)
+      {
+         by=((y/8)*84)+x;
+         bi=y % 8;
+
+         scrbuf[by+cx] &= ~(1<<bi);
+      }
+   }
+   LCD5110_update();
+}
+
+void LCD5110_drawVLine(int x, int y, int l)
+{
+   int by, bi;
+
+   if ((x>=0) && (x<84) && (y>=0) && (y<48))
+   {
+      for (int cy=0; cy<l; cy++)
+      {
+         setPixel(x, y+cy);
+      }
+   }
+   LCD5110_update();
+}
+
+void LCD5110_clrVLine(int x, int y, int l)
+{
+   int by, bi;
+
+   if ((x>=0) && (x<84) && (y>=0) && (y<48))
+   {
+      for (int cy=0; cy<l; cy++)
+      {
+         clrPixel(x, y+cy);
+      }
+   }
+   LCD5110_update();
+}
+
+void LCD5110_drawLine(int x1, int y1, int x2, int y2)
+{
+   int tmp;
+   double delta, tx, ty;
+   double m, b, dx, dy;
+
+   if (((x2-x1)<0))
+   {
+      tmp=x1;
+      x1=x2;
+      x2=tmp;
+      tmp=y1;
+      y1=y2;
+      y2=tmp;
+   }
+    if (((y2-y1)<0))
+   {
+      tmp=x1;
+      x1=x2;
+      x2=tmp;
+      tmp=y1;
+      y1=y2;
+      y2=tmp;
+   }
+
+   if (y1==y2)
+   {
+      if (x1>x2)
+      {
+         tmp=x1;
+         x1=x2;
+         x2=tmp;
+      }
+      LCD5110_drawHLine(x1, y1, x2-x1);
+   }
+   else if (x1==x2)
+   {
+      if (y1>y2)
+      {
+         tmp=y1;
+         y1=y2;
+         y2=tmp;
+      }
+      LCD5110_drawVLine(x1, y1, y2-y1);
+   }
+   else if (abs(x2-x1)>abs(y2-y1))
+   {
+      delta=((double)(y2-y1)/(double)(x2-x1));
+      ty=(double)(y1);
+      if (x1>x2)
+      {
+         for (int i=x1; i>=x2; i--)
+         {
+            setPixel(i, (int)(ty+0.5));
+              ty=ty-delta;
+         }
+      }
+      else
+      {
+         for (int i=x1; i<=x2; i++)
+         {
+            setPixel(i, (int)(ty+0.5));
+              ty=ty+delta;
+         }
+      }
+   }
+   else
+   {
+      delta=((float)(x2-x1)/(float)(y2-y1));
+      tx=(float)(x1);
+        if (y1>y2)
+        {
+         for (int i=y2+1; i>y1; i--)
+         {
+             setPixel((int)(tx+0.5), i);
+              tx=tx+delta;
+         }
+        }
+        else
+        {
+         for (int i=y1; i<y2+1; i++)
+         {
+             setPixel((int)(tx+0.5), i);
+              tx=tx+delta;
+         }
+        }
+   }
+   LCD5110_update();
+}
+
+void LCD5110_clrLine(int x1, int y1, int x2, int y2)
+{
+   int tmp;
+   double delta, tx, ty;
+   double m, b, dx, dy;
+
+   if (((x2-x1)<0))
+   {
+      tmp=x1;
+      x1=x2;
+      x2=tmp;
+      tmp=y1;
+      y1=y2;
+      y2=tmp;
+   }
+    if (((y2-y1)<0))
+   {
+      tmp=x1;
+      x1=x2;
+      x2=tmp;
+      tmp=y1;
+      y1=y2;
+      y2=tmp;
+   }
+
+   if (y1==y2)
+   {
+      if (x1>x2)
+      {
+         tmp=x1;
+         x1=x2;
+         x2=tmp;
+      }
+      clrHLine(x1, y1, x2-x1);
+   }
+   else if (x1==x2)
+   {
+      if (y1>y2)
+      {
+         tmp=y1;
+         y1=y2;
+         y2=tmp;
+      }
+      clrVLine(x1, y1, y2-y1);
+   }
+   else if (abs(x2-x1)>abs(y2-y1))
+   {
+      delta=((double)(y2-y1)/(double)(x2-x1));
+      ty=(double)(y1);
+      if (x1>x2)
+      {
+         for (int i=x1; i>=x2; i--)
+         {
+            clrPixel(i, (int)(ty+0.5));
+              ty=ty-delta;
+         }
+      }
+      else
+      {
+         for (int i=x1; i<=x2; i++)
+         {
+            clrPixel(i, (int)(ty+0.5));
+              ty=ty+delta;
+         }
+      }
+   }
+   else
+   {
+      delta=((float)(x2-x1)/(float)(y2-y1));
+      tx=(float)(x1);
+        if (y1>y2)
+        {
+         for (int i=y2+1; i>y1; i--)
+         {
+             clrPixel((int)(tx+0.5), i);
+              tx=tx+delta;
+         }
+        }
+        else
+        {
+         for (int i=y1; i<y2+1; i++)
+         {
+             clrPixel((int)(tx+0.5), i);
+              tx=tx+delta;
+         }
+        }
+   }
+   LCD5110_update();
+}
+
+void LCD5110_drawRect(int x1, int y1, int x2, int y2)
+{
+   int tmp;
+
+   if (x1>x2)
+   {
+      tmp=x1;
+      x1=x2;
+      x2=tmp;
+   }
+   if (y1>y2)
+   {
+      tmp=y1;
+      y1=y2;
+      y2=tmp;
+   }
+
+   drawHLine(x1, y1, x2-x1);
+   drawHLine(x1, y2, x2-x1);
+   drawVLine(x1, y1, y2-y1);
+   drawVLine(x2, y1, y2-y1+1);
+   LCD5110_update();
+}
+
+void LCD5110_clrRect(int x1, int y1, int x2, int y2)
+{
+   int tmp;
+
+   if (x1>x2)
+   {
+      tmp=x1;
+      x1=x2;
+      x2=tmp;
+   }
+   if (y1>y2)
+   {
+      tmp=y1;
+      y1=y2;
+      y2=tmp;
+   }
+
+   clrHLine(x1, y1, x2-x1);
+   clrHLine(x1, y2, x2-x1);
+   clrVLine(x1, y1, y2-y1);
+   clrVLine(x2, y1, y2-y1+1);
+}
+
+void LCD5110_drawRoundRect(int x1, int y1, int x2, int y2)
+{
+   int tmp;
+
+   if (x1>x2)
+   {
+      tmp=x1;
+      x1=x2;
+      x2=tmp;
+   }
+   if (y1>y2)
+   {
+      tmp=y1;
+      y1=y2;
+      y2=tmp;
+   }
+   if ((x2-x1)>4 && (y2-y1)>4)
+   {
+      setPixel(x1+1,y1+1);
+      setPixel(x2-1,y1+1);
+      setPixel(x1+1,y2-1);
+      setPixel(x2-1,y2-1);
+      LCD5110_drawHLine(x1+2, y1, x2-x1-3);
+      LCD5110_drawHLine(x1+2, y2, x2-x1-3);
+      LCD5110_drawVLine(x1, y1+2, y2-y1-3);
+      LCD5110_drawVLine(x2, y1+2, y2-y1-3);
+   }
+   LCD5110_update();
+}
+
+void LCD5110_clrRoundRect(int x1, int y1, int x2, int y2)
+{
+   int tmp;
+
+   if (x1>x2)
+   {
+      tmp=x1;
+      x1=x2;
+      x2=tmp;
+   }
+   if (y1>y2)
+   {
+      tmp=y1;
+      y1=y2;
+      y2=tmp;
+   }
+   if ((x2-x1)>4 && (y2-y1)>4)
+   {
+	   LCD5110_clrPixel(x1+1,y1+1);
+	   LCD5110_clrPixel(x2-1,y1+1);
+	   LCD5110_clrPixel(x1+1,y2-1);
+	   LCD5110_clrPixel(x2-1,y2-1);
+	   LCD5110_clrHLine(x1+2, y1, x2-x1-3);
+	   LCD5110_clrHLine(x1+2, y2, x2-x1-3);
+	   LCD5110_clrVLine(x1, y1+2, y2-y1-3);
+	   LCD5110_clrVLine(x2, y1+2, y2-y1-3);
+   }
+}
+
+void LCD5110_drawCircle(int x, int y, int radius)
+{
+   int f = 1 - radius;
+   int ddF_x = 1;
+   int ddF_y = -2 * radius;
+   int x1 = 0;
+   int y1 = radius;
+   char ch, cl;
+
+   setPixel(x, y + radius);
+   setPixel(x, y - radius);
+   setPixel(x + radius, y);
+   setPixel(x - radius, y);
+
+   while(x1 < y1)
+   {
+      if(f >= 0)
+      {
+         y1--;
+         ddF_y += 2;
+         f += ddF_y;
+      }
+      x1++;
+      ddF_x += 2;
+      f += ddF_x;
+      setPixel(x + x1, y + y1);
+      setPixel(x - x1, y + y1);
+      setPixel(x + x1, y - y1);
+      setPixel(x - x1, y - y1);
+      setPixel(x + y1, y + x1);
+      setPixel(x - y1, y + x1);
+      setPixel(x + y1, y - x1);
+      setPixel(x - y1, y - x1);
+   }
+   LCD5110_update();
+}
+
+void LCD5110_clrCircle(int x, int y, int radius)
+{
+   int f = 1 - radius;
+   int ddF_x = 1;
+   int ddF_y = -2 * radius;
+   int x1 = 0;
+   int y1 = radius;
+   char ch, cl;
+
+   LCD5110_clrPixel(x, y + radius);
+   LCD5110_clrPixel(x, y - radius);
+   LCD5110_clrPixel(x + radius, y);
+   LCD5110_clrPixel(x - radius, y);
+
+   while(x1 < y1)
+   {
+      if(f >= 0)
+      {
+         y1--;
+         ddF_y += 2;
+         f += ddF_y;
+      }
+      x1++;
+      ddF_x += 2;
+      f += ddF_x;
+      LCD5110_clrPixel(x + x1, y + y1);
+      LCD5110_clrPixel(x - x1, y + y1);
+      LCD5110_clrPixel(x + x1, y - y1);
+      LCD5110_clrPixel(x - x1, y - y1);
+      LCD5110_clrPixel(x + y1, y + x1);
+      LCD5110_clrPixel(x - y1, y + x1);
+      LCD5110_clrPixel(x + y1, y - x1);
+      LCD5110_clrPixel(x - y1, y - x1);
+   }
+   LCD5110_update();
+}
+
+void LCD5110_drawBitmap(int x, int y, uint8_t* bitmap, int sx, int sy)
+{
+   int bit;
+   char data;
+
+   for (int cy=0; cy<sy; cy++)
+   {
+      bit= cy % 8;
+      for(int cx=0; cx<sx; cx++)
+      {
+         data=bitmapbyte(cx+((cy/8)*sx));
+         if ((data & (1<<bit))>0)
+        	 setPixel(x+cx, y+cy);
+         else
+        	 LCD5110_clrPixel(x+cx, y+cy);
+      }
+   }
+}
